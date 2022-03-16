b2e288297984400f0a25ab4fa01d750d2ca28cd2
Eriry930 <erika-915@hotmail.com>
2022-03-16 12:11:07 -0500

    icons to svg converted to components

---
+++ src/assets/icons/Icon_work.jsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+const Icon_work = (props) => (
+  <svg
+    width={153}
+    height={153}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clipPath="url(#a)">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M91.748 31.122V15.856H61.215v15.266h30.533ZM15.415 46.39v83.967h122.133V46.389H15.415Zm122.133-15.267a15.214 15.214 0 0 1 15.267 15.267v83.967a15.214 15.214 0 0 1-15.267 15.266H15.415A15.213 15.213 0 0 1 .148 130.356l.076-83.967c0-8.473 6.718-15.267 15.19-15.267h30.534V15.856A15.214 15.214 0 0 1 61.215.589h30.533a15.214 15.214 0 0 1 15.267 15.267v15.266h30.533Z"
+        fill="#7D7D7D"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" transform="translate(.815 .59)" d="M0 0h152v152H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default Icon_work;
