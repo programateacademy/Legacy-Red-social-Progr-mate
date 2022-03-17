a7459c5106179c7a35400b89d998f40c0bac6a2f
FernandoDSanchez <fernandodendara@gmail.com>
2022-03-17 11:53:34 -0500

    conflicts resolved

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,59 +16,6 @@ import Icon_work from "../../assets/icons/Icon_work";
 const Navbar = () => {
     return (
     <header className={styles.globalNav}>
-<<<<<<< HEAD
-        <div className={styles.globalNavContent}>
-            <nav className={styles.navBar}>
-                <Link to="/home">
-                <img
-                    src={iconoEducamas}
-                    alt="Prográmate"
-                    className={styles.iconoEducamas}
-                />
-                </Link>
-                <ul className={styles.navList}>
-                <Link to="/home">
-                    <li className={styles.navListItem}>
-                    <BiHomeAlt size="30" />
-                    <p>Home</p>
-                    </li>
-                </Link>
-                <Link to="/community">
-                    <li className={styles.navListItem}>
-                    <BiGroup size="30" />
-                    <p>Comunidad</p>
-                    </li>
-                </Link>
-                <Link to="/questions">
-                    <li className={styles.navListItem}>
-                    <BiMessageRoundedAdd size="30" />
-                    <p>Foro</p>
-                    </li>
-                </Link>
-                </ul>
-                <div className={styles.containerPhoto}>
-                    <img
-                        src={logoperfil}
-                        alt="Prográmate"
-                        className={styles.photoProfile}
-                    />
-                    <DropdownLogOut />
-                </div>
-                <div>
-                    <Icon_news />
-                </div>
-                <div>
-                    <Icon_events />
-                </div>
-                <div>
-                    <Icon_forum />
-                </div>
-                <div>
-                    <Icon_work />
-                </div>
-            </nav>
-        </div>
-=======
       <div className={styles.globalNavContent}>
         <nav className={styles.navBar}>
           <Link to="/home">
@@ -111,7 +58,6 @@ const Navbar = () => {
           </div>
         </nav>
       </div>
->>>>>>> 36a9512d07d8cd009b69854a536d6b8c91e8b694
     </header>
 );
 };
