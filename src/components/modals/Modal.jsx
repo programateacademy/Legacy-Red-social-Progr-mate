0e35b279ebe3e2606e175914935f2f0d1fa4a996
FernandoDSanchez <fernandodendara@gmail.com>
2022-03-19 19:05:15 -0500

    Modals fixed

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Modal.css";
 
-const Modal = ({ children, isOpen, closeModal }) => {
+const Modal = ({ children, IsOpen, closeModal }) => {
   const handleModalContainerClick = (e) => e.stopPropagation();
-  console.log(isOpen)
   return (
-    <article className={`modal ${isOpen} is-open `} onClick={closeModal}>
+    <article className={`modal ${IsOpen && 'is-open'}  `} onClick={closeModal}>
       
       <div className="modal-container" onClick={handleModalContainerClick}>
         <button className="modal-close" onClick={closeModal}>
--- src/components/modals/Modals.jsx
+++ src/components/modals/Modals.jsx
@@ -6,15 +6,16 @@ import { useModals } from "./useModals.jsx";
 const Modals = () => {
   const [IsOpenModalJob, openModalJob, closeModalJob] = useModals(false);
   const [IsOpenModalEvent, openModalEvent, closeModalEvent] = useModals(false);
+  console.log(IsOpenModalJob)
   return (
     <div>
       <h2>Hay algo quequieras compartir?</h2>
       <button onClick={openModalEvent}>Evento </button>
       <Modal IsOpen={IsOpenModalEvent} closeModal={closeModalEvent}>
-        <h3> Evento</h3>
-        <p> hola este es el contenido de mi modal event </p>
+        <h3>Evento</h3>
+        <p>Hola este es el contenido de mi modal event</p>
       </Modal>
-      <button onClick={openModalJob}>Oferta de trabajo </button>
+      <button onClick={openModalJob}>Oferta de trabajo</button>
       <Modal IsOpen={IsOpenModalJob} closeModal={closeModalJob}>
         <FormJobs/>
       </Modal>
