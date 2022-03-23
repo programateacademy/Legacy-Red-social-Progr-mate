ae882c5dd287f84d7a00c5a01c6270438f167dec
HernandoSilvaLeal <hernandosilvaleal@gmail.com>
2022-03-23 10:00:35 -0500

    Inicio de revision de componentes admin a forms

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 
-export const baseUrl = 'https://redsocialprogramate.herokuapp.com';
+// export const baseUrl = 'https://redsocialprogramate.herokuapp.com';
+export const baseUrl = "http://localhost:3000";
 
