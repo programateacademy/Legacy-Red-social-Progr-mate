268ecdbc686d103a978f17c9a431c161c3cb7765
Cristian Garcia <cgarcia@caballo.local>
2022-12-13 07:30:18 -0500

    Changes 13/12/2022 Cohort V Post Module Se creo carpeta de test

---
+++ src/test/TextBox.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Name from '../components/ProfileEditor/formInfo/Name';
+
+describe('TextBox component', () => {
+    test('Probando test', () => {
+      render(<Name/>);
+
+      const inputNode = screen.getByLabelText('Username')
+
+    })
+    
+});
