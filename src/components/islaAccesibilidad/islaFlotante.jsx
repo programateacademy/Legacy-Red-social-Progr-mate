b662e2b32ddac83d29649bd31dedd6946e55265a
Jefferson Pulido <jeffersonpulido55@mail.com>
2022-12-15 06:22:02 -0500

    Changes 14/12/2022 Cohort V Componente Accesibilidad v1.2

--- src/components/islaAccesibilidad/islaFlotante.jsx
+++ src/components/islaAccesibilidad/islaFlotante.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import style from "./";
+import style from './isla.module.css'
 
 const islaFlotante = () => {
   return (
-    <div>islaFlotante</div>
+    <>
+      <div className={style.contenedor}>
+        <button className={style.botonF1} id="btnF1" >
+          <span><i class="fa-solid fa-sun"></i></span>
+        </button>
+      </div>
+    </>
   )
 }
 
