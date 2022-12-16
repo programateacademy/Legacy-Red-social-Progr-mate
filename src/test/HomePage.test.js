dbd555224af89baa86a27e4b583dc3d4f336ff74
Jefferson Pulido <jeffersonpulido55@gmail.com>
2022-12-16 11:53:05 -0500

    Changes 16/12/2022 Cohort V Testing FAILED v1.3

--- src/test/HomePage.test.js
+++
@@ -1,9 +0,0 @@
-import HomePage from "../pages/HomePage";
-import { shallow } from "enzyme";
-
-describe("Tests para el home de la aplicación", () => {
-  it("Deberia renderizarse sin Error", () => {
-    const wrapper = shallow(<HomePage />);
-    expect(wrapper).toHaveLength(1);
-  });
-});
